0ec84a68bcce20767de8ca2883fc6b62e7c9f1f7
springercass <casprin2@gmail.com>
2019-07-17 20:48:45 -0500

    crud working, no null

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,15 +1,57 @@
 const schemeDb = require("../data/dbconfig");
 
 module.exports = {
-  find
-  //   findById,
-  //   findSteps,
-  //   add,
-  //   addStep,
-  //   update,
-  //   remove
+  find,
+  findById,
+  findSteps,
+  add,
+  addStep,
+  update,
+  remove
 };
 
 function find() {
   return schemeDb("schemes");
+}
+
+function findById(id) {
+  return schemeDb("schemes")
+    .where("id", id)
+    .first();
+}
+
+function findSteps(id) {
+  return schemeDb("steps")
+    .where("scheme_id", id)
+    .orderBy("step_number");
+}
+
+function findStepById(id) {
+  return schemeDb("steps")
+    .where("id", id)
+    .first();
+}
+
+function add(scheme) {
+  return schemeDb("schemes")
+    .insert(scheme)
+    .then(([id]) => findById(id));
+}
+
+function addStep(step, schemeID) {
+  return schemeDb("steps")
+    .insert({ ...step, scheme_id: schemeID })
+    .then(([stepID]) => findStepById(stepID));
+}
+
+function update(scheme_name, id) {
+  return schemeDb("schemes")
+    .where("id", id)
+    .update(scheme_name);
+}
+
+function remove(id) {
+  return schemeDb("schemes")
+    .where("id", id)
+    .del();
 }
